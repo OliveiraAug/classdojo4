be7ecae77b1b5d30075c409def2c60cb6b523be3
Augusto Oliveira <augustoo@edu.univali.br>
2022-06-01 12:02:38 -0300

    Inicio da quarta versão do projeto Class Dojo pelos instrutores Augusto, Felipe, Jules e Murilo.

---
+++ backend/src/main/java/com/instrutores/classdojo4/Classdojo4Application.java
@@ -0,0 +1,13 @@
+package com.instrutores.classdojo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Classdojo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Classdojo4Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/instrutores/classdojo4/Classdojo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instrutores.classdojo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Classdojo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
